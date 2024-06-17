cd0f7041ab19ac05717da889f1f50c917388737f
Jose Manuel Cortes Ceron <230110688@itsoeh.edu.mx>
2024-06-17 14:18:37 -0600

    Update: create the GUI

--- App/main.py
+++ App/main.py
@@ -1,8 +1,10 @@
-from kivy.lang import Builder
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
 
 class YoutubeDownloaderApp(App):
     def build(self):
+        Window.size = (550, 140)
         return Builder.load_file('youtubedownloader.kv')
 
 if __name__ == '__main__':
